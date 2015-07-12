4aca5850563f45b8f1fb9a706f8399d2ac6fae65
Sacheth <sacheg3@gmail.com>
2015-07-12 00:46:38 -0700

    Added simple GUI

---
+++ src/GUI.java
@@ -0,0 +1,25 @@
+/**
+ * Used to initialize the graphing application to be run.
+ */
+
+// Import statements
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ * @author sachethhegde
+ * This class runs the application to start the graphing.
+ */
+public class GUI extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
